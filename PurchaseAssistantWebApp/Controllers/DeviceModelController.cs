88f1dde09a6bb305ee54bbd4ef2240b65b6cdf0e
Tripti Saini <triptisaini123@gmail.com>
2020-10-14 18:45:57 +0530

    Add search controller

---
+++ PurchaseAssistantWebApp/Controllers/DeviceModelController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PurchaseAssistantWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceModelController : ControllerBase
+    {
+        // Return particular model by model number
+        [HttpGet("{modelName}")]
+        public string Get(string modelName)
+        {
+            return modelName;
+        }
+    }
+}
---
+++ PurchaseAssistantWebApp/Controllers/SearchController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PurchaseAssistantWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        // Returns list of all monitors
+        public string Get()
+        {
+            string result = "all monitors";
+            return result;
+        }
+
+        // Returns list of monitors based on monitor type(primary or additional)
+        [HttpGet("{monitorType}")]
+        public string Get(string monitorType)
+        {
+            return monitorType;
+        }
+
+        // Returns list of monitors based on monitor type and portability
+        [HttpGet("{monitorType}/{portability}")]
+        public string Get(string monitorType, string portability)
+        {
+            return monitorType + portability;
+        }
+
+        // Returns list of monitors based on monitor type, portability and display size
+        [HttpGet("{monitorType}/{portability}/{displaySize}")]
+        public string Get(string monitorType, string portability, string displaySize)
+        {
+            return monitorType + portability + displaySize;
+        }
+
+        // Returns list of monitors based on monitor type, portability, series and series name
+        [HttpGet("{monitorType}/{portability}/{series}/{seriesName}")]
+        public string Get(string monitorType, string portability, string series, string seriesName)
+        {
+            return monitorType + portability + series + seriesName;
+        }
+
+    }
+}
