d5bdf7980cff08541e66b2b9404d2f46659acb52
Tripti Saini <triptisaini123@gmail.com>
2020-10-19 15:03:25 +0530

    Add integration tests for sales rep controller

---
+++ PurchaseAssistantBackend.Test/IntegrationTests/SalesRepresentativeControllerTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net;
+using PurchaseAssistantWebApp.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace PurchaseAssistantBackend.Test.IntegrationTests
+{
+    public class SalesRepresentativeControllerTest
+    {
+        private readonly TestProgram program;
+        private static string url = "http://localhost:5000/api/SalesRepresentative";
+
+        public SalesRepresentativeControllerTest()
+        {
+            program = new TestProgram();
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnAllSalesRepresentativesWithHttpStatusOk()
+        {
+            var response = await program.Client.GetAsync(url);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_WhenValidRequestSentThenAddSuccessfullyAndReturnHttpsStatusOk()
+        {
+            SalesRepresentative newSalesRepresentative = new SalesRepresentative { Id = "SR002", Name = "Ellie", Email = "ellie@gmail.com", DepartmentRegion = "France" };
+
+            var response = await program.Client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(newSalesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_WhenInvalidRequestSentThenReturnHttpsStatusBadRequest()
+        {
+            SalesRepresentative newSalesRepresentative = new SalesRepresentative { Id = "SR002", Name = "Ellie", Email = "ellie@gmail.com"};
+
+            var response = await program.Client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(newSalesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Put_WhenValidRequestSentThenUpdateSuccessfullyAndReturnHttpsStatusOk()
+        {
+            // First add a new sales representative
+            SalesRepresentative newSalesRepresentative = new SalesRepresentative { Id = "SR0042", Name = "Ellie", Email = "ellie@gmail.com", DepartmentRegion = "France" };
+
+            var response = await program.Client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(newSalesRepresentative), Encoding.UTF8, "application/json"));
+
+            // Then update and check result
+            SalesRepresentative salesRepresentative = new SalesRepresentative { Id = "SR0042", Name = "Ellie", Email = "ellie@gmail.com", DepartmentRegion = "Germany" };
+
+            response = await program.Client.PutAsync(url, new StringContent(JsonConvert.SerializeObject(salesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task Put_WhenInvalidRequestWithNonExistentKeySentThenReturnHttpsStatusBadRequest()
+        {
+            SalesRepresentative salesRepresentative = new SalesRepresentative { Id = "SR0099", Name = "Ellie", Email = "ellie@gmail.com", DepartmentRegion = "Germany" };
+
+            var response = await program.Client.PutAsync(url, new StringContent(JsonConvert.SerializeObject(salesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Put_WhenInvalidRequestWithMissingEmailSentThenReturnHttpsStatusBadRequest()
+        {
+            SalesRepresentative salesRepresentative = new SalesRepresentative { Id = "SR0042", Name = "Ellie", DepartmentRegion = "Germany" };
+
+            var response = await program.Client.PutAsync(url, new StringContent(JsonConvert.SerializeObject(salesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Delete_WhenValidIdSentThenDeleteSuccessfullyAndReturnHttpsStatusOk()
+        {
+            // First add a new sales representative
+            SalesRepresentative newSalesRepresentative = new SalesRepresentative { Id = "SR0042", Name = "Ellie", Email = "ellie@gmail.com", DepartmentRegion = "France" };
+
+            var response = await program.Client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(newSalesRepresentative), Encoding.UTF8, "application/json"));
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+            // Then delete and check result
+
+            response = await program.Client.DeleteAsync(url + "/SR0042");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task Delete_WhenInvalidRequestWithNonExistentKeySentThenReturnHttpsStatusBadRequest()
+        {
+
+            var response = await program.Client.DeleteAsync(url + "/SR0097");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ PurchaseAssistantBackend.Test/TestProgram.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using PurchaseAssistantWebApp;
+
+namespace PurchaseAssistantBackend.Test
+{
+    public class TestProgram
+    {
+        public HttpClient Client { get; private set; }
+        private TestServer _server;
+        public TestProgram()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = _server.CreateClient();
+        }
+  
+    }
+}
