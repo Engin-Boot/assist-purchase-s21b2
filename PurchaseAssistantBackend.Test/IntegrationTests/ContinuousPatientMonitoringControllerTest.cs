d9f048d79e6f918f0b4d5611a91f77618e1da9cd
Tripti Saini <triptisaini123@gmail.com>
2020-10-19 15:21:43 +0530

    Add test cases for ContinuousPatientMonitoringSystems controller

---
+++ PurchaseAssistantBackend.Test/IntegrationTests/ContinuousPatientMonitoringControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PurchaseAssistantBackend.Test.IntegrationTests
+{
+    public class ContinuousPatientMonitoringControllerTest
+    {
+        private readonly TestProgram program;
+        private static string url = "http://localhost:5000/api/ContinuousPatientMonitoringSystems";
+
+        public ContinuousPatientMonitoringControllerTest()
+        {
+            program = new TestProgram();
+        }
+
+        [Fact]
+        public async Task Get_WhenQueryHasValidParameterValuesThenReturnFilteredModelsWithHttpStatusOk()
+        {
+            string query = "Portability=true";
+
+            var response = await program.Client.GetAsync(url + "?" + query);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_WhenQueryHasInvalidParameterValuesThenReturnFilteredModelsWithHttpStatusBadRequest()
+        {
+            string query = "Portability=any";
+
+            var response = await program.Client.GetAsync(url + "?" + query);
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
