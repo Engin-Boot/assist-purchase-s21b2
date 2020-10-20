043a5d0161fe9a4090d7244dcc055505b509aba6
swati98 <37444333+swati98@users.noreply.github.com>
2020-10-20 13:40:02 +0530

    Update SearchQuery.cs

--- PurchaseAssistantWebApp/Models/SearchQuery.cs
+++ PurchaseAssistantWebApp/Models/SearchQuery.cs
@@ -9,5 +9,13 @@
         public string BatterySupport { get; set; }
         public string MultiPatientSupport { get; set; }
         public string TouchScreenSupport { get; set; }
+        
+        // clinical parameters
+        public string BpCheck { get; set; }
+        public string HeartRateCheck { get; set; }
+        public string EcgCheck { get; set; }
+        public string SpO2Check { get; set; }
+        public string TemperatureCheck { get; set; }
+        public string CardiacOutputCheck { get; set; }
     }
 }
