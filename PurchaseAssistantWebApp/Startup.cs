5201f5d8c23279ec3daee822aafdf5071895c716
Ajay Kumar <41167768+ajay300898@users.noreply.github.com>
2020-10-26 19:00:15 +0530

    Corrected database path

--- PurchaseAssistantWebApp/Startup.cs
+++ PurchaseAssistantWebApp/Startup.cs
@@ -22,7 +22,7 @@ namespace PurchaseAssistantWebApp
             {
                 //options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
                 //options.UseSqlite("Data source= C:/Users/Ajay kumar/source/repos/assist-purchase-s21b2/PurchaseAssistantWebApp/PurchaseAssistant.db");
-                options.UseSqlite("Data source= D:\a\assist-purchase-s21b2\assist-purchase-s21b2\PurchaseAssistantWebApp\PurchaseAssistant.db");
+                options.UseSqlite("Data source= D:/a/assist-purchase-s21b2/assist-purchase-s21b2/PurchaseAssistantWebApp/PurchaseAssistant.db");
             });
             services.AddScoped<Repository.IModelsSpecificationDataRepository, Repository.ModelsSpecificationDataRepository>();
             services.AddScoped<Repository.ICallSetupRequestDataRepository, Repository.CallSetupRequestDataRepository>();
