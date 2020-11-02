1941208f35966e59a3fec4e6af1029a3ec8f0214
Ajay Kumar <ajay300898@gmail.com>
2020-11-02 08:12:06 +0530

    Removed Unused Usings

--- AssistToPurchase/Converters/ModelsObjectToId.cs
+++ AssistToPurchase/Converters/ModelsObjectToId.cs
@@ -1,8 +1,6 @@
 ﻿using AssistToPurchase.Model;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 
 namespace AssistToPurchase.Converters
--- AssistToPurchase/ViewModel/SalesRepresentativeViewModel.cs
+++ AssistToPurchase/ViewModel/SalesRepresentativeViewModel.cs
@@ -2,15 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using AlertToCareFrontend.Command;
 using AssistToPurchase.Model;
-using Newtonsoft.Json;
 
 using RestSharp;
 using RestSharp.Serialization.Json;
